f83fd1201d800a5f68a6872488af498ae4a6a56a
Ji3xiangLi <daisy121024@gmail.com>
2023-05-31 14:29:38 +0800

    successfully infrenced

--- starcoder_infrence.py
+++ starcoder_infrence.py
@@ -1,4 +1,4 @@
-from typing import Union, List, Tuple, Optional
+from typing import Tuple
 import torch
 from huggingface_hub import snapshot_download
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
@@ -29,22 +29,15 @@ class Inference:
         raw_model.tie_weights()
 
         # Load the checkpoint and dispatch the model to the specified devices
+        # no_split_module_class = ["GPTJBlock"]  的意思是modules为GPTJBlock的都不会拆分到多个设备上，这里所有需要残差计算的模型都应该写上去。
         model = load_checkpoint_and_dispatch(
             raw_model,
             checkpoint=snapshot_download(self.checkpoint),
             device_map="auto",
-            no_split_module_classes=["GPTBigCodeBlock"],
-            # no_split_module_class = ["GPTJBlock"]  的意思是modules为GPTJBlock的都不会拆分到多个设备上，这里所有需要残差计算的模型都应该写上去。
-            dtype=torch.float16
-        )
+            no_split_module_classes=["GPTBigCodeBlock"],  # 根据模型结构修改
+            dtype=torch.float16)
 
         return model
-
-    def preprocess(self, raw_text: str) -> Tuple[torch.Tensor, torch.Tensor]:
-        text = raw_text
-        tokens = self.tokenizer.batch_encode_plus([text], return_tensors="pt")
-        input_ids = tokens['input_ids']
-        return input_ids
 
     def forward(self, input_text: str):
         inputs = self.tokenizer(input_text, return_tensors="pt")
@@ -57,20 +50,7 @@ class Inference:
 
 
 if __name__ == "__main__":
-    #import os
-    #os.environ["CUDA_VISIBLE_DEVICES"] = "4,5,6,7"
-
-    # Create an Inference instance with the specified model directory.
     infer = Inference()
-
-    # ！！！如果需要运行量化版本，请以以下方式load模型！！！
-    # If you need to load a quantized model, please instead load the model and then pass it into Inference.__init__.
-    # model = MossForCausalLM.from_pretrained("fnlp/moss-moon-003-sft-int4").half().cuda()
-    # infer = Inference(model, device_map="auto")
-
-    # Define a test case string.
-    test_case = "def hello_world():"
-
-    # Generate a response using the Inference instance.
+    test_case = "please write a quick sort code"
     res = infer(test_case)
     print(res)
